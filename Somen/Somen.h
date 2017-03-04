41326881f2dc31122bcc23db4d7e9b2aaaa7ed84
Yuya Hirayama <hiragram@hiragram.local>
2017-03-04 22:14:18 +0900

    Initial Commit

---
+++ Somen/Somen.h
@@ -0,0 +1,19 @@
+//
+//  Somen.h
+//  Somen
+//
+//  Created by Yuya Hirayama on 2017/03/04.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Somen.
+FOUNDATION_EXPORT double SomenVersionNumber;
+
+//! Project version string for Somen.
+FOUNDATION_EXPORT const unsigned char SomenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Somen/PublicHeader.h>
+
+
