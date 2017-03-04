4b8c7a4d3a3d7c7f937735be968e1d141d8d65b2
Yuya Hirayama <hiragram@hiragram.local>
2017-03-04 22:16:03 +0900

    Initial commit

---
+++ SomenSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomenSample
+//
+//  Created by Yuya Hirayama on 2017/03/04.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
