934c616e876b46d7bac4eaf4de5fd5d8c8f60c9d
Yuya Hirayama <y@yura.me>
2017-03-05 15:27:30 +0900

    Add test

---
+++ Somen/Event.swift
@@ -0,0 +1,89 @@
+//
+//  Event.swift
+//  Somen
+//
+//  Created by Yuya Hirayama on 2017/03/05.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import Foundation
+
+// TODO: make sure to support all events
+
+public enum Event {
+
+  public typealias RawEvent = [String: Any]
+
+  case newStatus(rawEvent: RawEvent)
+  case deleteStatus(rawEvent: RawEvent)
+  case deleteLocation(rawEvent: RawEvent)
+  case limitNotice(rawEvent: RawEvent)
+  case statusWithheld(rawEvent: RawEvent)
+  case userWithheld(rawEvent: RawEvent)
+  case stallWarning(rawEvent: RawEvent)
+  case userUpdate(rawEvent: RawEvent)
+  case friends(rawEvent: RawEvent)
+  case directMessage(rawEvent: RawEvent)
+  case block(rawEvent: RawEvent)
+  case unblock(rawEvent: RawEvent)
+  case favorite(rawEvent: RawEvent)
+  case unfavorite(rawEvent: RawEvent)
+  case follow(rawEvent: RawEvent)
+  case unfollow(rawEvent: RawEvent)
+  case listCreated(rawEvent: RawEvent)
+  case listDestroyed(rawEvent: RawEvent)
+  case listUpdated(rawEvent: RawEvent)
+  case listMemberAdded(rawEvent: RawEvent)
+  case listMemberRemoved(rawEvent: RawEvent)
+  case listUserSubscribed(rawEvent: RawEvent)
+  case listUserUnsubscribed(rawEvent: RawEvent)
+  case quotedTweet(rawEvent: RawEvent)
+
+  init?(rawEvent: RawEvent) {
+    if rawEvent.keys.contains("delete") {
+      self = .deleteStatus(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("scrub_get") {
+      self = .deleteLocation(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("limit") {
+      self = .limitNotice(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("status_withheld") {
+      self = .statusWithheld(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("user_withheld") {
+      self = .userWithheld(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("warning") {
+      self = .stallWarning(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "user_update" {
+      self = .userUpdate(rawEvent: rawEvent)
+    } else if rawEvent.keys.contains("friends") {
+      self = .friends(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "block" {
+      self = .block(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "unblock" {
+      self = .unblock(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "favorite" {
+      self = .favorite(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "unfavorite" {
+      self = .unfavorite(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "follow" {
+      self = .follow(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "unfollow" {
+      self = .unfollow(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_created" {
+      self = .listCreated(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_destroyed" {
+      self = .listDestroyed(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_updated" {
+      self = .listUpdated(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_member_added" {
+      self = .listMemberAdded(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_memver_removed" {
+      self = .listMemberRemoved(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_user_subscribed" {
+      self = .listUserSubscribed(rawEvent: rawEvent)
+    } else if rawEvent["event"] as? String == "list_user_unsubscribed" {
+      self = .listUserUnsubscribed(rawEvent: rawEvent)
+    } else {
+      return nil
+    }
+  }
+}
--- SomenTests/SomenTests.swift
+++ SomenTests/SomenTests.swift
@@ -7,30 +7,87 @@
 //
 
 import XCTest
+import Nimble
 @testable import Somen
 
 class SomenTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func test_generateEnumFromJsonString() {
+    let dict = getDict(fromJSONString: "{\"delete\":{\"status\":{\"id\":1234,\"id_str\":\"1234\",\"user_id\":3,\"user_id_str\":\"3\"}}}")
+
+    guard let event = Event.init(rawEvent: dict) else {
+      XCTFail()
+      return
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    expect(event.hashValue).to(equal(Event.deleteStatus(rawEvent: [:]).hashValue))
+  }
+
+  func getDict(fromJSONString json: String) -> Event.RawEvent {
+    return try! JSONSerialization.jsonObject(with: json.data(using: .utf8)!, options: []) as! Event.RawEvent
+  }
+}
+
+private extension Event {
+  var hashValue: Int {
+    switch self {
+    case .newStatus:
+      return 1
+    case .deleteStatus:
+      return 2
+    case .deleteLocation:
+      return 3
+    case .limitNotice:
+      return 4
+    case .statusWithheld:
+      return 5
+    case .userWithheld:
+      return 6
+    case .stallWarning:
+      return 7
+    case .userUpdate:
+      return 8
+    case .friends:
+      return 9
+    case .directMessage:
+      return 10
+    case .block:
+      return 11
+    case .unblock:
+      return 12
+    case .favorite:
+      return 13
+    case .unfavorite:
+      return 14
+    case .follow:
+      return 15
+    case .unfollow:
+      return 16
+    case .listCreated:
+      return 17
+    case .listDestroyed:
+      return 18
+    case .listUpdated:
+      return 19
+    case .listMemberAdded:
+      return 20
+    case .listMemberRemoved:
+      return 21
+    case .listUserSubscribed:
+      return 22
+    case .listUserUnsubscribed:
+      return 23
+    case .quotedTweet:
+      return 24
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
 }
